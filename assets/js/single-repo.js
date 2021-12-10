d331ec0ccb8ad1e149d8775d903f62358c0ced48
Maverick <Mav33987@gmail.com>
2021-12-09 18:42:34 -0600

    finished single-repo page

--- assets/js/single-repo.js
+++ assets/js/single-repo.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var displayIssues = function(issues) {
 
@@ -47,6 +48,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -56,5 +61,19 @@ var getRepoIssues = function(repo) {
     
   };
 
+ 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
   
-  getRepoIssues("MaverickCavazos/javascript-quiz");
+    // create link element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+  
+  getRepoIssues("facebook/react");
